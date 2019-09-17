de8b5237a799320d61ece9d2654eb4fa613a1eaa
feyaq13 <feyaq13@gmail.com>
2019-09-17 11:34:14 +0300

    добавляет окружение и зависимости для разработки

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+
+function style () { // компилирует файл scss в css
+  return gulp.src('./src/scss/**/*.scss')
+    .pipe(sass()) // процесс компиляции
+    .pipe(postcss([ autoprefixer() ]))
+    .pipe(gulp.dest('./src')) // создает файл в './src'
+    .pipe(browserSync.stream()); // запускает browser-sync
+}
+
+function watch () { // смотрит только за изменениями файлов
+  browserSync.init({ // инициализирует сервер
+    server: {
+      baseDir: './src'
+    },
+    online: true,
+    tunnel: true,
+    logLevel: "debug"
+  });
+  gulp.watch('./src/scss/**/*.scss', style);
+  gulp.watch('./src/*.js').on('change', browserSync.reload);
+  gulp.watch('./src/*.html').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
+
+// запуск 'gulp watch' через терминал или 'npm start'
