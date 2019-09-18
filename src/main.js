9cc0e7987604e9aae7c5fd495532630b0ce61d33
feyaq13 <feyaq13@gmail.com>
2019-09-18 17:57:50 +0300

    доделывает хедер

--- src/main.js
+++ src/main.js
@@ -1,31 +1,12 @@
-function setupPaddingTop () {
-  const dimensionHeightNavigation = document.querySelector('.header__navigation').offsetHeight;
-  document.querySelector('.header-page__main-headerings').style.paddingTop = dimensionHeightNavigation + 'px';
+function setupMarginTop () {
+  const dimensionHeightNavigation = document.querySelector('.header-page').offsetHeight;
+  document.querySelector('.header-page__main-content').style.marginTop = (dimensionHeightNavigation / 2) + 'px';
 }
 
 function setupInheritWidth () {
   const widthParent = document.querySelector('.header-page').offsetWidth + 'px'
   const child = document.querySelector('.header__navigation')
   child.style.maxWidth = widthParent;
-}
-
-function init () {
-  const btnNavTrigger = document.querySelector('.btn-nav-trigger')
-  setupPaddingTop()
-  setupInheritWidth()
-  btnNavTrigger.addEventListener('click', handlerMenu)
-  btnNavTrigger.addEventListener('mouseenter', enterHamburger)
-  btnNavTrigger.addEventListener('mouseleave', leaveHamburger)
-}
-
-function handlerMenu () {
-  const navList = document.querySelector('.navigation__list')
-
-  if (navList.classList.contains('open')) {
-    navList.classList.replace('open', 'close')
-  } else {
-    navList.classList.replace('close', 'open')
-  }
 }
 
 function enterHamburger () {
@@ -36,7 +17,32 @@ function leaveHamburger () {
   this.children[2].style.width = "";
 }
 
+function handlerMenu (e) {
+  let target = e.target;
+  const navBar = document.querySelector('.header__navigation')
+  const navList = document.querySelector('.navigation__list')
+
+
+  if (navBar.contains(target) && !target.classList.contains('phone')) {
+    if (navList.classList.contains('open')) {
+      navList.classList.replace('open', 'close')
+    } else {
+      navList.classList.replace('close', 'open')
+    }
+  } else {
+    navList.classList.replace('open', 'close')
+  }
+}
+
+function init () {
+  const btnNavTrigger = document.querySelector('.btn-nav-trigger')
+
+  setupMarginTop()
+  setupInheritWidth()
+
+  btnNavTrigger.addEventListener('mouseenter', enterHamburger)
+  btnNavTrigger.addEventListener('mouseleave', leaveHamburger)
+  document.addEventListener('click', handlerMenu)
+}
+
 init()
-
-
-
