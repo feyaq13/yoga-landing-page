d317c7e2256b75419a2bc66d5cf7a5d704e27d06
feyaq13 <feyaq13@gmail.com>
2019-09-18 09:27:09 +0300

    правит скрипт сборщика

--- gulpfile.js
+++ gulpfile.js
@@ -10,26 +10,26 @@ function style () { // компилирует файл scss в css
   return gulp.src('./src/scss/**/*.scss')
     .pipe(sass()) // процесс компиляции
     .pipe(postcss([ autoprefixer() ]))
-    .pipe(gulp.dest('./src/docs')) // создает файл в './src'
+    .pipe(gulp.dest('./docs')) // создает файл в './src'
     .pipe(browserSync.stream()); // запускает browser-sync
 }
 
 function move () { // переносит файлы в неизменном виде в директорию для docs
-  gulp.src('./src/*.{html,js}').pipe(gulp.dest('./src/docs'))
-  gulp.src('./src/images/*').pipe(gulp.dest('./src/docs/images'))
+  gulp.src('./src/*.{html,js}').pipe(gulp.dest('./docs'))
+  gulp.src('./src/images/*').pipe(gulp.dest('./docs/images'))
 }
 
 function watch () { // смотрит только за изменениями файлов
   browserSync.init({ // инициализирует сервер
     server: {
-      baseDir: './src'
+      baseDir: './docs'
     },
     online: true,
     tunnel: true,
   });
   gulp.watch('./src/scss/**/*.scss', style);
-  gulp.watch('./src/*.js').on('change', browserSync.reload);
-  gulp.watch('./src/*.html').on('change', browserSync.reload);
+  gulp.watch('./src/*.{html,js}').on('change', move);
+  gulp.watch('./docs/**/*').on('change', browserSync.reload);
 }
 
 // todo сделать копировалку для index.html main.js и картинок в docs в неизменном виде
