76b479bf48794efb8eb875609514e452ace93271
Semyon Fomin <taxnuke@gmail.com>
2019-10-05 23:13:33 +0300

    правит кодстайл

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ const jQueryBridget = require('jquery-bridget');
 jQueryBridget('flickity', Flickity, $);
 
 const $teamCarousel = $('.section-our-team__carousel-team').flickity({
-  cellAlign: "center",
+  cellAlign: 'center',
   draggable: true,
   pageDots: false,
   resize: true,
@@ -13,7 +13,7 @@ const $teamCarousel = $('.section-our-team__carousel-team').flickity({
   setGallerySize: false
 });
 
-const flkty = $teamCarousel.data('flickity')
+const flkty = $teamCarousel.data('flickity');
 const $teamCarouselStatus = $('</p>').addClass('carousel-team__carousel-status');
 let childrenSlider = $('.section-our-team__carousel-team .flickity-slider').children();
 
@@ -23,47 +23,46 @@ const $reviewsCarousel = $('.section-reviews__carousel-reviews').flickity({
   draggable: true,
   pageDots: false,
   adaptiveHeight: true,
-  setGallerySize: false,
-})
+  setGallerySize: false
+});
 
-const flkty2 = $reviewsCarousel.data('flickity')
+const flkty2 = $reviewsCarousel.data('flickity');
 const $reviewsCarouselStatus = $('</p>').addClass('carousel-reviews__carousel-status');
 
-$(document).ready(function() {
-  $(".navigation__list").on("click","a", function (event) {
+$(document).ready(function () {
+  $('.navigation__list').on('click', 'a', function (event) {
     event.preventDefault();
 
-    let listItem  = $(this).attr('href')
-    let top = $(listItem).offset().top
+    const listItem = $(this).attr('href');
+    const top = $(listItem).offset().top;
 
-    $('body,html').animate({scrollTop: top}, 1000);
-  })
+    $('body,html').animate({ scrollTop: top }, 1000);
+  });
 });
 
 (function () {
-  const btnNavTrigger = document.querySelector('.btn-nav-trigger')
-  btnNavTrigger.addEventListener('mouseenter', enterHamburger)
-  btnNavTrigger.addEventListener('mouseleave', leaveHamburger)
+  const btnNavTrigger = document.querySelector('.btn-nav-trigger');
+  btnNavTrigger.addEventListener('mouseenter', enterHamburger);
+  btnNavTrigger.addEventListener('mouseleave', leaveHamburger);
   document.addEventListener('click', handlerMenu);
-  $teamCarousel.on('select.flickity', updateStatus)
-  $reviewsCarousel.on('select.flickity', updateStatus)
+  $teamCarousel.on('select.flickity', updateStatus);
+  $reviewsCarousel.on('select.flickity', updateStatus);
 
   $(window).on('load resize', function () {
-    setupMarginTop()
-    setupInheritWidth()
+    setupMarginTop();
+    setupInheritWidth();
 
     if (window.matchMedia('(min-width: 769px)').matches) {
-      showGroupingCells()
+      showGroupingCells();
     } else if (window.matchMedia('(max-width: 768px)').matches) {
-      showRegroupingCells()
+      showRegroupingCells();
     }
 
-    updateStatus()
-    $('.section-our-team .flickity-viewport').outerHeight($('.carousel-cell').outerHeight())
-    $('.section-reviews .flickity-viewport').outerHeight($('.review').outerHeight() + 20)
-  })
-
-})()
+    updateStatus();
+    $('.section-our-team .flickity-viewport').outerHeight($('.carousel-cell').outerHeight());
+    $('.section-reviews .flickity-viewport').outerHeight($('.review').outerHeight() + 20);
+  });
+})();
 
 function setupMarginTop () {
   const dimensionHeightNavigation = document.querySelector('.header-page').offsetHeight;
@@ -71,51 +70,49 @@ function setupMarginTop () {
 }
 
 function setupInheritWidth () {
-  const widthParent = document.querySelector('.header-page').offsetWidth + 'px'
-  const child = document.querySelector('.header-page__navigation')
+  const widthParent = document.querySelector('.header-page').offsetWidth + 'px';
+  const child = document.querySelector('.header-page__navigation');
   child.style.maxWidth = widthParent;
 }
 
 function enterHamburger () {
-  this.children[2].style.width = "50%";
+  this.children[2].style.width = '50%';
 }
 
 function leaveHamburger () {
-  this.children[2].style.width = "";
+  this.children[2].style.width = '';
 }
 
 function handlerMenu (e) {
-  let target = e.target;
-  const navBar = document.querySelector('.header-page__navigation')
-  const navList = document.querySelector('.navigation__list')
+  const target = e.target;
+  const navBar = document.querySelector('.header-page__navigation');
+  const navList = document.querySelector('.navigation__list');
 
   if (navBar.contains(target) && !target.classList.contains('phone')) {
     if (navList.classList.contains('open')) {
-      navList.classList.replace('open', 'close')
+      navList.classList.replace('open', 'close');
     } else {
-      navList.classList.replace('close', 'open')
+      navList.classList.replace('close', 'open');
     }
   } else {
-    navList.classList.replace('open', 'close')
+    navList.classList.replace('open', 'close');
   }
 }
 
 function showGroupingCells () {
-  const cellFragment = $('<div class="carousel-cell"></div>')
+  const cellFragment = $('<div class="carousel-cell"></div>');
 
   if (childrenSlider.length === 0) {
-    childrenSlider = $('.section-our-team__carousel-team .flickity-slider').children()
-
+    childrenSlider = $('.section-our-team__carousel-team .flickity-slider').children();
   } else if (childrenSlider.length > 0 && $('.carousel-cell').length !== 4) {
+    flkty.cells.map(function (cell) {
+      cell.element.classList.value = 'cell';
+      return cell.element;
+    });
 
-    flkty.cells.map(function(cell) {
-      cell.element.classList.value = 'cell'
-      return cell.element
-    })
-
-    const detachedChildren = $(childrenSlider.splice(0, 3)).detach()
-    let slide = cellFragment.append(detachedChildren)
-    $('.section-our-team__carousel-team .flickity-slider').append(slide)
+    const detachedChildren = $(childrenSlider.splice(0, 3)).detach();
+    const slide = cellFragment.append(detachedChildren);
+    $('.section-our-team__carousel-team .flickity-slider').append(slide);
 
     // for (let i = 0; i < childrenSlider.length; i++) {
     //   $(childrenSlider[i]).addClass('cell').removeClass('carousel-cell');
@@ -127,39 +124,35 @@ function showGroupingCells () {
     // // console.log(slide)
     // $('.section-our-team__carousel-team .flickity-slider').append(slide)
 
-    return showGroupingCells()
+    return showGroupingCells();
   }
 }
 
-function showRegroupingCells() {
-
+function showRegroupingCells () {
   if (childrenSlider.length > 0 && $('.carousel-cell').length !== 10) {
-    const DetachedChildren = $('.carousel-cell').children().detach()
-    $('.carousel-cell').detach()
-    $('.section-our-team__carousel-team .flickity-slider').append(DetachedChildren)
+    const DetachedChildren = $('.carousel-cell').children().detach();
+    $('.carousel-cell').detach();
+    $('.section-our-team__carousel-team .flickity-slider').append(DetachedChildren);
 
     for (let i = 0; i < $($('.flickity-slider').children()).length; i++) {
-      $($('.section-our-team__carousel-team .flickity-slider').children()[i]).removeClass('cell').addClass('carousel-cell')
+      $($('.section-our-team__carousel-team .flickity-slider').children()[i]).removeClass('cell').addClass('carousel-cell');
     }
 
-    childrenSlider = $('.section-our-team__carousel-team .flickity-slider').children()
-    return childrenSlider
+    childrenSlider = $('.section-our-team__carousel-team .flickity-slider').children();
+    return childrenSlider;
   }
 }
 
-function updateStatus() {
-  const cellNumber = flkty.selectedIndex + 1
-  const cellNumber2 = flkty2.selectedIndex + 1
-  const flickityBtn = $('.section-our-team__carousel-team .flickity-button.flickity-prev-next-button.previous')
-  const flickityBtn2 = $('.section-reviews .flickity-button.flickity-prev-next-button.previous')
+function updateStatus () {
+  const cellNumber = flkty.selectedIndex + 1;
+  const cellNumber2 = flkty2.selectedIndex + 1;
+  const flickityBtn = $('.section-our-team__carousel-team .flickity-button.flickity-prev-next-button.previous');
+  const flickityBtn2 = $('.section-reviews .flickity-button.flickity-prev-next-button.previous');
 
-  $teamCarousel.flickity('reloadCells')
-  $reviewsCarousel.flickity('reloadCells')
-  $teamCarouselStatus.text(cellNumber + '/' + flkty.slides.length)
-  $reviewsCarouselStatus.text(cellNumber2 + '/' + flkty2.slides.length)
-  flickityBtn.after($teamCarouselStatus)
-  flickityBtn2.after($reviewsCarouselStatus)
+  $teamCarousel.flickity('reloadCells');
+  $reviewsCarousel.flickity('reloadCells');
+  $teamCarouselStatus.text(cellNumber + '/' + flkty.slides.length);
+  $reviewsCarouselStatus.text(cellNumber2 + '/' + flkty2.slides.length);
+  flickityBtn.after($teamCarouselStatus);
+  flickityBtn2.after($reviewsCarouselStatus);
 }
-
-
-
